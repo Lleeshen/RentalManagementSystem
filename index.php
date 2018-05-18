73aecb41f1ead9608be089baff281c186792ca46
lyman l shen <lshen@linux60419.dc.engr.scu.edu>
2018-05-17 20:05:29 -0700

    Setup Heading

--- index.php
+++ index.php
@@ -7,26 +7,28 @@ if(!$conn) {
         exit;
 }
 //PHP stuff
-echo '
+echo "
     <!DOCTYPE html>
     <html lang='en'>
         <head>
-            <title>Bootstrap Example</title>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-        <\head>
+            <meta charset='utf-8'>
+            <title>HappyRenter - Home</title>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css'>
+            <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'> </script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js'> </script>
+            <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js'> </script>
+        </head>
         <body>
-            <div class="jumbotron jumbotron-fluid">
-                <h1> HappyRenter\'s Rental Management Inc.<\h1>
-            <\div>
+            <div class='jumbotron jumbotron-fluid'>
+                <div class='container'>
+                    <h2 class='display-3'> HappyRenter's Rental Management Inc. </h2>
+                </div>
+            </div>
+        </body>
                 
-        <\body>
     </html>
-'
+";
 // Log off
 OCILogoff($conn);
 ?>
