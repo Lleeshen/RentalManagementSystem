a0fc34bb8131bd61bccd072effc185c767f596ba
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-05-28 18:09:24 -0700

    Set up SQL to create table

---
+++ sql_files/addTables.sql
@@ -0,0 +1,12 @@
+
+/*
+Branch(branchId, phone, address)
+ */
+CREATE TABLE Branch (
+    branchId VARCHAR(5);
+    Phone VARCHAR(10);
+    /* Address */
+    Street VARCHAR(15);
+    City VARCHAR(10);
+    Zip VARCHAR(10);
+)
