706e6fd58395581b8fd7155ff64f33a62e0f2e8b
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-05-29 11:05:50 -0700

    The sql commands will actually work now

--- sql_files/addTables.sql
+++ sql_files/addTables.sql
@@ -1,13 +1,13 @@
+/* Address is Street, City, Zip for all tables */
+
 /* Branch(branchId, phone, address) */
 CREATE TABLE Branch (
     branchId VARCHAR(5) PRIMARY KEY,
     Phone VARCHAR(15),
-    /* Address is Street, City, Zip */
     Street VARCHAR(15),
     City VARCHAR(10),
     Zip VARCHAR(10)
-)
-
+);
 /* Employee(empId, name, phone, job_designation) */
 CREATE TABLE Employee (
     empId VARCHAR(5) PRIMARY KEY,
@@ -35,7 +35,6 @@ CREATE TABLE Supervisor (
 /* Property_Owner(name, permanent address, phone) */
 CREATE TABLE Property_Owner (
     name VARCHAR(10),
-    /* Address is Street, City, Zip */
     Street VARCHAR(15),
     City VARCHAR(10),
     Zip VARCHAR(10),
@@ -45,11 +44,10 @@ CREATE TABLE Property_Owner (
 /* Rental_Property(rental_num, owner, address, number of rooms, monthly rent, start date of availability, phonoe, supId) */
 CREATE TABLE Rental_Property (
     rental_num INTEGER PRIMARY KEY,
-    status BOOLEAN,
-    /* Address is Street, City, Zip */
+    status INTEGER,
     Street VARCHAR(15),
     City VARCHAR(10),
-    Zip VARCHAR(10)
+    Zip VARCHAR(10),
     num_rooms INTEGER,
     monthly_rent NUMERIC(5,2),
     start_date_of_availibility DATE,
@@ -58,14 +56,12 @@ CREATE TABLE Rental_Property (
     Foreign key (owner_phone) references Property_Owner (phone),
     Foreign key (supId) references Supervisor (empid)
 );
-
 /* Renter(renter_name, home_phone, work_phone) */
 CREATE TABLE Renter (
     name VARCHAR(10),
     home_phone VARCHAR(15),
     work_phone VARCHAR(15) PRIMARY KEY
 );
-
 /* Lease_Agreement(rental_num, start_lease_date, end_lease_date, renter_home_phone,
 renter_work_phone, deposit_amt, rent_amt, supervisor_name) */
 CREATE TABLE Lease_Agreement (
@@ -78,5 +74,5 @@ CREATE TABLE Lease_Agreement (
     sup_name VARCHAR(10),
     PRIMARY KEY (rental_num,start_date),
     Foreign Key (rental_num) references Rental_Property (rental_num),
-    Foreign Key (renter_wphone) references Renter (work_phone),
+    Foreign Key (renter_wphone) references Renter (work_phone)
 );
