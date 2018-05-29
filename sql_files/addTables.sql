d88406c5439f216ab5c4593a10ec7c7565def61e
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-05-28 18:13:36 -0700

    Added Employee to Create Table SQL

--- sql_files/addTables.sql
+++ sql_files/addTables.sql
@@ -1,12 +1,17 @@
-
-/*
-Branch(branchId, phone, address)
- */
+/* Branch(branchId, phone, address) */
 CREATE TABLE Branch (
-    branchId VARCHAR(5);
-    Phone VARCHAR(10);
-    /* Address */
-    Street VARCHAR(15);
-    City VARCHAR(10);
-    Zip VARCHAR(10);
+    branchId VARCHAR(5),
+    Phone VARCHAR(15),
+    /* Address is Street, City, Zip */
+    Street VARCHAR(15),
+    City VARCHAR(10),
+    Zip VARCHAR(10)
 )
+
+/* Employee(empId, name, phone, job_designation) */
+CREATE TABLE Employee (
+    empId VARCHAR(5),
+    name VARCHAR(10),
+    phone VARCHAR(15),
+    job_designation VARCHAR(15)
+);
