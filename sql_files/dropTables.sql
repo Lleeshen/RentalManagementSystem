b0e59b25543cbd031835ea89bff428500d8accb2
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-05-29 21:12:53 -0700

    Easier to drop and re-add table with this

---
+++ sql_files/dropTables.sql
@@ -0,0 +1,15 @@
+DROP TABLE Lease_Agreement;
+
+DROP TABLE Renter;
+
+DROP TABLE Rental_Property;
+
+DROP TABLE Property_Owner;
+
+DROP TABLE Supervisor;
+
+DROP TABLE Manager;
+
+DROP TABLE Employee;
+
+DROP TABLE Branch;
