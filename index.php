64b3fc4812338c6ac312ff1650ae1628800d927d
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-05-29 21:10:44 -0700

    Set up layout for GUI

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if(!$conn) {
         print htmlentities($e['message']);
         exit;
 }
-//PHP stuff
+//If renter/owner/leaseAgreement submited through form, insert appropiate one
 // Log off
 oci_close($conn);
 ?>
@@ -26,9 +26,54 @@ oci_close($conn);
             <div class="container">
                 <h3 class="display-3"> HappyRenter's Rental Management Inc. </h3>
                 <br />
-                <p> We manage rental properties for land owners </p>
+                <p> We manage rental properties for land owners and renters. </p>
             </div>
         </div>
+        <div class="container">
+            <h4 class="display-4"> We have many operations to help you </h4>
+            <p> 
+                Are you a new property owner, or a new renter? Choose the option to get registered. <br />
+                Are you already an owner or renter? There is also an option to create an agreement.
+            </p>
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                    Choose item to insert
+                </button>
+                <div class="dropdown-menu">
+                    <!-- Will need links to forms to insert only those things -->
+                    <a class="dropdown-item" href="#">Get registered as property owner</a>
+                    <a class="dropdown-item" href="#">Get registered as renter</a>
+                    <a class="dropdown-item" href="#">Make a lease agreement</a>
+                </div>
+            </div>
+            <br /> 
+            <p>
+                Want information about properties?
+                Choose your desired operation.
+            </p>
+            <!-- Links to this page -->
+            <form method="post" action="#">
+                <div class="form-group">
+                    <label for="action"> Select operation </label>
+                    <select class="form-control" id="action" name="oplist">
+                        <option id="op1"> Show availible properties </option>
+                        <option id="op2"> Show Lease Agreements expiring soon </option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Do this action! </button>
+            </form>
+        </div>
+        <?php
+            $conn=oci_connect('username','password','//dbserver.engr.scu.edu/db11g');
+            if(!$conn) {
+                $e = oci_error();
+                print "<br> connection failed:";
+                print htmlentities($e['message']);
+                exit;
+            }
+            //Show Table operations
+            oci_close($conn);
+        ?>
     </body>   
 </html>
 
