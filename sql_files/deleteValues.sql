0ee96dbcf3d310170c7e61d7cea3d4eaa26e6ce8
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-06-03 14:12:36 -0700

    Able to reset tables to no values

---
+++ sql_files/deleteValues.sql
@@ -0,0 +1,15 @@
+DELETE FROM Lease_Agreement;
+
+DELETE FROM Renter;
+
+DELETE FROM Rental_Property;
+
+DELETE FROM Property_Owner;
+
+DELETE FROM Supervisor;
+
+DELETE FROM Manager;
+
+DELETE FROM Employee;
+
+DELETE FROM Branch;
