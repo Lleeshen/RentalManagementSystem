1d6b81fa7362173073b1cd822b0dc25c06e36a6c
lyman l shen <lshen@linux60810.dc.engr.scu.edu>
2018-06-03 22:29:25 -0700

    Another constraint done

--- sql_files/constraints.sql
+++ sql_files/constraints.sql
@@ -1,4 +1,4 @@
-/*
+
 CREATE OR REPLACE TRIGGER LeaseAgreementLength
 AFTER INSERT OR UPDATE ON Lease_Agreement
 FOR EACH ROW
@@ -8,11 +8,13 @@ BEGIN
     DayDiff := :new.end_date - :new.start_date;
     IF (DayDiff < 180 OR DayDiff > 366)
 THEN
-    ROLLBACK;
+    raise_application_error(-20000,'Agreement must be between 6 months and 1 year');
 END IF;
 END;
 /
-*/
+show errors;
+
+
 /* When  a  lease  agreement  is  created,  the  status  for  the  property  should  be  changed  to leased. */
 CREATE OR REPLACE TRIGGER LeaseAgreementStatusUpdate
 AFTER INSERT ON Lease_Agreement
@@ -25,5 +27,6 @@ END;
 /
 
 /*
-insert into lease_agreement values (7,'2405409801',TO_DATE('2018-01-01','YY-MM-DD'),TO_DATE('2018-07-01','YY-MM-DD'),200,300,'Anna');
+insert into lease_agreement values (7,'2405409801',TO_DATE('2018-01-01','YY-MM-DD'),TO_DATE('2018-02-01','YY-MM-DD'),200,300,'Anna');
 */
+
