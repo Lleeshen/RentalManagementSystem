85068c50af24bd875b6655c0538e39120fb70bcb
lyman l shen <lshen@linux60814.dc.engr.scu.edu>
2018-06-04 14:18:13 -0700

    Only the last renter of property has rent increased

--- sql_files/constraints.sql
+++ sql_files/constraints.sql
@@ -20,29 +20,31 @@ CREATE Or Replace TRIGGER increaseRent
 
     newRentalNum    INTEGER;
     newRentalDate   DATE;
-    dateToChange    DATE
+    dateToChange    DATE;
 
     BEFORE EACH ROW IS
     BEGIN
         newRentalNum := :new.rental_num;
         newRentalDate:= :new.start_date;
-        /*
         SELECT MAX(start_date)
         INTO dateToChange
         FROM Lease_Agreement
         WHERE rental_num = newRentalNum
             AND start_date < newRentalDate;
-        */
     END BEFORE EACH ROW;
 
     AFTER STATEMENT IS
     BEGIN
         UPDATE Lease_Agreement
         SET rent_amt = rent_amt * 1.1
-        WHERE rental_num = newRentalNum AND start_date <> newRentalDate;
+        WHERE rental_num = newRentalNum AND start_date = dateToChange;
     END AFTER STATEMENT;
     
 END increaseRent;
 /
 show errors;
 
+/* Test wtih
+Select * FROM Lease_Agreement;
+INSERT INTO Lease_Agreement VALUES(8,'4327785567',TO_DATE('2018-03-01','YY-MM-DD'),TO_DATE('2018-10-01','YY-MM-DD'),200.0,300.0,'Anna');
+SELECT * FROM Lease_Agreement
